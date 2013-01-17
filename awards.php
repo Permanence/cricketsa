57758a07541b0767ff71cf8da3d14343fe7aca9c
Permanence <permanence84@gmail.com>
2013-01-16 21:34:21 -0800

    Update awards.php

--- awards.php
+++ awards.php
@@ -1,61 +1,119 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<!--<title>Awards | Cricket Lovers South Australia</title>-->
-<title>Awards for Talented Young Cricketers in South Australia | Cricket Lovers South Australia</title>
-<meta name="Description" content="Cricket Lovers SA recognise and reward the best young cricketers in South Australia because cricket is important to us." />
-<meta name="Keywords" content="Cricket Awards, Best Cricketer, Cricketer of the Year, Best and Fairest Cricketer, Cricket Champion,  Cricket legend, Cricket Hero, Cricket Fan, Adelaide Oval, South Australia" />
-<?php include('includes/header.php'); ?>
-	<div id="contents" class="cf">
-		<h2>Awards</h2>
-		<p>Cricket Lovers (SA) seeks to encourage young South Australian cricketers by presenting an Annual Young Male Cricketer of the Year Award (Gerald Fishpool Memorial Trophy) and an Annual Young Female Cricketer of the Year Award.</p>
-		<p class="mb30">The recipients of the awards are chosen by the Cricket Lovers Committee.</p>
-			<table summary="Young Cricketer of the Year Award" cellspacing="0" class="table2">
-				<tr><th>2012 Young Male Cricketer of the Year</th><td>Travis Head (Tea Tree Gully C.C.)</td></tr>
-				<tr><th>2012 Young Female Cricketer of the Year</th><td>Tahlia McGrath (Southern Districts C.C.)</td></tr>
-				<tr><th>2012 Male Under 18 Encouragement Award</th><td>Kelvin Smith (West Torrens C.C.)</td></tr>
-				<tr><th>2012 Female Under 18 Encouragement Award</th><td>Alexandra Price (Sturt C.C.)</td></tr>
-			</table>
-
-		<h3 id="pastrecipients">Past Recipients</h3>
-			<table summary="Young Cricketer of the Year Award" cellspacing="0" class="table3">
-				<tr><th>2011</th><td class="bold">Kane Richardson (Male)</td><td>(East Torrens C.C.)</td></tr>
-				<tr><th>2011</th><td class="bold">Megan Schutt (Female)</td><td>(Sturt C.C.)</td></tr>
-				<tr><th>2011</th><td class="bold">Travis Head (Male Under 18)</td><td>(Tea Tree Gully C.C.)</td></tr>
-				<tr><th>2011</th><td class="bold">Jessica O'Reilly (Female Under 18)</td><td>(Southern Districts)</td></tr>
-				<tr><th>2010</th><td class="bold">James Smith (Male)</td><td>(West Torrens C.C.)</td></tr>
-				<tr><th>2010</th><td class="bold">Stephanie Morrison (Female)</td><td>(Sturt C.C.)</td></tr>
-				<tr><th>2009</th><td class="bold">Peter George (Male)</td><td>(West Torrens C.C.)</td></tr>
-				<tr><th>2009</th><td class="bold">Sarah-Jane Jameson (Female)</td><td>(Kensington C.C.)</td></tr>
- 				<tr><th>2008</th><td class="bold">Andy Delmont (Male)</td><td>(Adelaide University)</td></tr>
-				<tr><th>2008</th><td class="bold">Julie Woerner (Female)</td><td>(West Torrens C.C.)</td></tr>
-				<tr><th>2007</th><td class="bold">Jason Borgas (Male)</td><td>(Sturt C.C.)</td></tr>
-				<tr><th>2007</th><td class="bold">Lisa Wundenberg (Female)</td><td>(Northern Districts C.C.)</td></tr>
-				<tr><th>2006</th><td class="bold">Cullen Bailey</td><td>(Sturt C.C.)</td></tr>
-				<tr><th>2005</th><td class="bold">Daniel Cullen</td><td>(Adelaide C.C.)</td></tr>
-				<tr><th>2004</th><td class="bold">Ben Cameron</td><td>(Tea Tree Gully C.C.)</td></tr>
-				<tr><th>2003</th><td class="bold">Shaun Tait</td><td>(Sturt C.C.)</td></tr>
-				<tr><th>2002</th><td class="bold">Callum Ferguson</td><td>(Prospect C.C.)</td></tr>
-				<tr><th>2001</th><td class="bold">Paul Rofe</td><td>(East Torrens C.C.)</td></tr>
-				<tr><th>2000</th><td class="bold">Luke Williams</td><td>(Adelaide C.C.)</td></tr>
-				<tr><th>1999</th><td class="bold">Daniel Harris</td><td>(Woodville C.C.)</td></tr>
-				<tr><th>1998</th><td class="bold">Graham Manou</td><td>(Northern Districts C.C.)</td></tr>
-				<tr><th>1997</th><td class="bold">Nathan Adcock</td><td>(Sturt C.C.)</td></tr>
-				<tr><th>1996</th><td class="bold">Chris Davies</td><td>(Southern Districts C.C.)</td></tr>
-				<tr><th>1995</th><td class="bold">Anthony McDonald</td><td>(Kensington C.C.)</td></tr>
-				<tr><th>1994</th><td class="bold">Brad Young</td><td>(Port Adelaide C.C.)</td></tr>
-				<tr><th>1993</th><td class="bold">Brett Swain</td><td>(Adelaide University)</td></tr>
-				<tr><th>1992</th><td class="bold">Cameron Williamson</td><td>(Adelaide University)</td></tr>
-				<tr><th>1991</th><td class="bold">Tim Neilsen</td><td>(West Torrens C.C.)</td></tr>
-				<tr><th>1990</th><td class="bold">James Orchard</td><td>(Sturt C.C.)</td></tr>
-				<tr><th>1989</th><td class="bold">Gary Wright</td><td>(Prospect C.C.)</td></tr>
-				<tr><th>1988</th><td class="bold">Chris McGowan</td><td>(Adelaide University)</td></tr>
-			</table>
-		
-		<p><a href="membership.php"><img src="common/images/how_to_join_off.jpg" width="860" height="352" alt="How To Join Cricket Lovers welcomes and encourages new members. Members enjoy 1.Hearing interesting cricket identities (approx 6 times a year, generally at the Regal Park Motor Inn, Barton Terrace East, North Adelaide). 2.Annual Dinner 3.Quiz Night. To find out more about joining see the Membership page." /></a></p>
-	<!-- END contents --></div>
-<!-- END wrapper --></div>
-<?php include('includes/footer.php'); ?>
-</body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<!--<title>Awards | Cricket Lovers South Australia</title>-->
+
+<title>Awards for Talented Young Cricketers in South Australia | Cricket Lovers South Australia</title>
+
+<meta name="Description" content="Cricket Lovers SA recognise and reward the best young cricketers in South Australia because cricket is important to us." />
+
+<meta name="Keywords" content="Cricket Awards, Best Cricketer, Cricketer of the Year, Best and Fairest Cricketer, Cricket Champion,  Cricket legend, Cricket Hero, Cricket Fan, Adelaide Oval, South Australia" />
+
+<?php include('includes/header.php'); ?>
+
+	<div id="contents" class="cf">
+
+		<p>Cricket Lovers (SA) seeks to encourage young South Australian cricketers by presenting an Annual Young Male Cricketer of the Year Award (Gerald Fishpool Memorial Trophy) and an Annual Young Female Cricketer of the Year Award.</p>
+
+		<p class="mb30">The recipients of the awards are chosen by the Cricket Lovers Committee.</p>
+
+			<table summary="Young Cricketer of the Year Award" cellspacing="0" class="table2">
+
+				<tr><th>2012 Young Male Cricketer of the Year</th><td>Travis Head (Tea Tree Gully C.C.)</td></tr>
+
+				<tr><th>2012 Young Female Cricketer of the Year</th><td>Tahlia McGrath (Southern Districts C.C.)</td></tr>
+
+				<tr><th>2012 Male Under 18 Encouragement Award</th><td>Kelvin Smith (West Torrens C.C.)</td></tr>
+
+				<tr><th>2012 Female Under 18 Encouragement Award</th><td>Alexandra Price (Sturt C.C.)</td></tr>
+
+			</table>
+
+
+
+		<h3 id="pastrecipients">Past Recipients</h3>
+
+			<table summary="Young Cricketer of the Year Award" cellspacing="0" class="table3">
+
+				<tr><th>2011</th><td class="bold">Kane Richardson (Male)</td><td>(East Torrens C.C.)</td></tr>
+
+				<tr><th>2011</th><td class="bold">Megan Schutt (Female)</td><td>(Sturt C.C.)</td></tr>
+
+				<tr><th>2011</th><td class="bold">Travis Head (Male Under 18)</td><td>(Tea Tree Gully C.C.)</td></tr>
+
+				<tr><th>2011</th><td class="bold">Jessica O'Reilly (Female Under 18)</td><td>(Southern Districts)</td></tr>
+
+				<tr><th>2010</th><td class="bold">James Smith (Male)</td><td>(West Torrens C.C.)</td></tr>
+
+				<tr><th>2010</th><td class="bold">Stephanie Morrison (Female)</td><td>(Sturt C.C.)</td></tr>
+
+				<tr><th>2009</th><td class="bold">Peter George (Male)</td><td>(West Torrens C.C.)</td></tr>
+
+				<tr><th>2009</th><td class="bold">Sarah-Jane Jameson (Female)</td><td>(Kensington C.C.)</td></tr>
+
+ 				<tr><th>2008</th><td class="bold">Andy Delmont (Male)</td><td>(Adelaide University)</td></tr>
+
+				<tr><th>2008</th><td class="bold">Julie Woerner (Female)</td><td>(West Torrens C.C.)</td></tr>
+
+				<tr><th>2007</th><td class="bold">Jason Borgas (Male)</td><td>(Sturt C.C.)</td></tr>
+
+				<tr><th>2007</th><td class="bold">Lisa Wundenberg (Female)</td><td>(Northern Districts C.C.)</td></tr>
+
+				<tr><th>2006</th><td class="bold">Cullen Bailey</td><td>(Sturt C.C.)</td></tr>
+
+				<tr><th>2005</th><td class="bold">Daniel Cullen</td><td>(Adelaide C.C.)</td></tr>
+
+				<tr><th>2004</th><td class="bold">Ben Cameron</td><td>(Tea Tree Gully C.C.)</td></tr>
+
+				<tr><th>2003</th><td class="bold">Shaun Tait</td><td>(Sturt C.C.)</td></tr>
+
+				<tr><th>2002</th><td class="bold">Callum Ferguson</td><td>(Prospect C.C.)</td></tr>
+
+				<tr><th>2001</th><td class="bold">Paul Rofe</td><td>(East Torrens C.C.)</td></tr>
+
+				<tr><th>2000</th><td class="bold">Luke Williams</td><td>(Adelaide C.C.)</td></tr>
+
+				<tr><th>1999</th><td class="bold">Daniel Harris</td><td>(Woodville C.C.)</td></tr>
+
+				<tr><th>1998</th><td class="bold">Graham Manou</td><td>(Northern Districts C.C.)</td></tr>
+
+				<tr><th>1997</th><td class="bold">Nathan Adcock</td><td>(Sturt C.C.)</td></tr>
+
+				<tr><th>1996</th><td class="bold">Chris Davies</td><td>(Southern Districts C.C.)</td></tr>
+
+				<tr><th>1995</th><td class="bold">Anthony McDonald</td><td>(Kensington C.C.)</td></tr>
+
+				<tr><th>1994</th><td class="bold">Brad Young</td><td>(Port Adelaide C.C.)</td></tr>
+
+				<tr><th>1993</th><td class="bold">Brett Swain</td><td>(Adelaide University)</td></tr>
+
+				<tr><th>1992</th><td class="bold">Cameron Williamson</td><td>(Adelaide University)</td></tr>
+
+				<tr><th>1991</th><td class="bold">Tim Neilsen</td><td>(West Torrens C.C.)</td></tr>
+
+				<tr><th>1990</th><td class="bold">James Orchard</td><td>(Sturt C.C.)</td></tr>
+
+				<tr><th>1989</th><td class="bold">Gary Wright</td><td>(Prospect C.C.)</td></tr>
+
+				<tr><th>1988</th><td class="bold">Chris McGowan</td><td>(Adelaide University)</td></tr>
+
+			</table>
+
+		
+
+		<p><a href="membership.php"><img src="common/images/how_to_join_off.jpg" width="860" height="352" alt="How To Join Cricket Lovers welcomes and encourages new members. Members enjoy 1.Hearing interesting cricket identities (approx 6 times a year, generally at the Regal Park Motor Inn, Barton Terrace East, North Adelaide). 2.Annual Dinner 3.Quiz Night. To find out more about joining see the Membership page." /></a></p>
+
+	<!-- END contents --></div>
+
+<!-- END wrapper --></div>
+
+<?php include('includes/footer.php'); ?>
+
+</body>
+
 </html>
